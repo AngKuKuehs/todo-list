81c6eabdfd8457c567053439c5827b5ea1934bfb
Bryan Ang <bryanang2001@gmail.com>
2024-01-11 18:56:40 +0800

    implemented remove, update, and add routes

--- app.py
+++ app.py
@@ -39,16 +39,45 @@ def get_one_reminder():
 
 @app.route('/add', methods=['POST'])
 def add_reminder():
-    pass
+    db = client.todolist
+    reminders_collection = db.reminders_test
+    title = request.args.get('title')
+    new_reminder = {
+        "title": title,
+        "completed": False
+    }
+    result = reminders_collection.insert_one(new_reminder)
+    return f"_id of inserted document: {result.inserted_id}"
 
-@app.route('/add', methods=['PUT'])
+@app.route('/update', methods=['PUT'])
 def edit_reminder():
-    pass
+    db = client.todolist
+    reminders_collection = db.reminders_test
+    oid = request.args.get('oid')
+    title = request.args.get('title')
+    completed = request.args.get('completed')
+
+    reminder_to_update = {"_id": ObjectId(oid)}
+    updates = {}
+    if title:
+        updates["title"] = title
+    if completed != None:
+        updates["completed"] = completed
+    
+    result = reminders_collection.update_one(reminder_to_update, {"$set": updates})
+
+    return f"Documents updated: {str(result.modified_count)}"
+
 
 @app.route('/remove', methods=['DELETE'])
 def remove_reminder():
-    pass
+    db = client.todolist
+    reminders_collection = db.reminders_test
+    oid = request.args.get('oid')
+    document_to_delete = {"_id": ObjectId(oid)}
 
+    result = reminders_collection.delete_one(document_to_delete)
+    return f"Documents deleted: {str(result.deleted_count)}"
 
 
 if __name__ == "__main__":
